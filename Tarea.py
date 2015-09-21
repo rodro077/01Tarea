ec12ff1e0929ca02322498d11bb045b4b0b23380
hfehrmann <hans.jfehrmann@gmail.com>
2015-09-21 18:01:49 -0300

    avance

--- Tarea.py
+++ Tarea.py
@@ -4,12 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pylab import *
 datos = np.loadtxt('sun_AM0.dat')
-i = arange(0, len(datos), 1)
-x = datos[i-1]
-y = datos[i]
-plot(x[i],y[i])
-print i
+cantidad_datos = len(datos)
+x = np.zeros(cantidad_datos)
+y = np.zeros(cantidad_datos)
+for i in range(0,len(datos)):
+	x[i] = datos[i][0]
+	y[i] = datos[i][1]
 
+plot(np.log(x),y)
 xlabel('time (s)')
 ylabel('voltage (mV)')
 title('About as simple as it gets, folks')
